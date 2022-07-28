93d5397eaa027dce55a25c1baddc05df8d30e3d8
Bitok85 <kan041185@gmail.com>
2022-07-28 09:46:52 +0300

    4. Thymeleaf, Циклы. [#504841 #326402]

--- src/main/java/ru/job4j/dream/model/Post.java
+++ src/main/java/ru/job4j/dream/model/Post.java
@@ -1,5 +1,7 @@
 package ru.job4j.dream.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
 public class Post {
@@ -7,12 +9,12 @@ public class Post {
     private int id;
     private String name;
     private String description;
-    private String created;
+    private LocalDateTime created;
 
     public Post() {
     }
 
-    public Post(int id, String name, String description, String created) {
+    public Post(int id, String name, String description, LocalDateTime created) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -43,11 +45,11 @@ public class Post {
         this.description = description;
     }
 
-    public String getCreated() {
+    public LocalDateTime getCreated() {
         return created;
     }
 
-    public void setCreated(String created) {
+    public void setCreated(LocalDateTime created) {
         this.created = created;
     }
 
--- src/main/java/ru/job4j/dream/store/PostStore.java
+++ src/main/java/ru/job4j/dream/store/PostStore.java
@@ -2,6 +2,7 @@ package ru.job4j.dream.store;
 
 import ru.job4j.dream.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -11,9 +12,9 @@ public class PostStore {
     private static final PostStore INST = new PostStore();
     private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
     private PostStore() {
-        posts.put(1, new Post(1, "Junior Java Job", "some text", "04.06.2022"));
-        posts.put(2, new Post(2, "Middle Java Job", "some text", "11.07.2022"));
-        posts.put(3, new Post(3, "Senior Java Job", "some text", "17.05.2022"));
+        posts.put(1, new Post(1, "Junior Java Job", "some text", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "some text", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "some text", LocalDateTime.now()));
     }
 
     public static PostStore instOf() {
