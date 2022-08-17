8686716867c6a326adb7591664e73ed269f06774
Bitok85 <kan041185@gmail.com>
2022-08-17 10:57:22 +0300

    2. CandidateDbStore. Хранение кандидатов. [#504861]

--- db/scripts/update.sql
+++ db/scripts/update.sql
@@ -1,5 +1,12 @@
-CREATE TABLE post (
+CREATE TABLE IF NOT EXISTS post (
    id SERIAL PRIMARY KEY,
+   name varchar(255),
+   description varchar(255),
+   created timestamp
+);
+
+CREATE TABLE IF NOT EXISTS candidate (
+   id SERIAL PRIMARY KEy,
    name varchar(255),
    description varchar(255),
    created timestamp
---
+++ src/main/java/ru/job4j/dream/store/CandidateDBStore.java
@@ -0,0 +1,100 @@
+package ru.job4j.dream.store;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.stereotype.Repository;
+import ru.job4j.dream.model.Candidate;
+
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class CandidateDBStore {
+
+    private final BasicDataSource pool;
+
+    public CandidateDBStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public List<Candidate> findAll() {
+        List<Candidate> candidates = new ArrayList<>();
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate")
+        ) {
+            try (ResultSet resultSet = ps.executeQuery()) {
+                while (resultSet.next()) {
+                    candidates.add(new Candidate(resultSet.getInt("id"),
+                            resultSet.getString("name"),
+                            resultSet.getString("description"),
+                            resultSet.getTimestamp("created").toLocalDateTime()));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return candidates;
+    }
+
+    public Candidate add(Candidate candidate) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("INSERT INTO candidate(name, description, created) VALUES(?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)
+        ) {
+            ps.setString(1, candidate.getName());
+            ps.setString(2, candidate.getDescription());
+            ps.setTimestamp(3, Timestamp.valueOf(candidate.getCreated()));
+            ps.execute();
+            try (ResultSet id = ps.getGeneratedKeys()) {
+                if (id.next()) {
+                    candidate.setId(id.getInt(1));
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return candidate;
+    }
+
+    public boolean update(Candidate candidate) {
+        boolean result = false;
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps =
+                     cn.prepareStatement("UPDATE candidate SET name = ?, description = ?, created = ?")
+        ) {
+            ps.setString(1, candidate.getName());
+            ps.setString(2, candidate.getDescription());
+            ps.setTimestamp(3, Timestamp.valueOf(candidate.getCreated()));
+            result = ps.executeUpdate() > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public Candidate findById(int id) {
+        try (Connection cn = pool.getConnection();
+             PreparedStatement ps = cn.prepareStatement("SELECT * FROM candidate WHERE id = ?")
+        ) {
+            ps.setInt(1, id);
+            try (ResultSet it = ps.executeQuery()) {
+                if (it.next()) {
+                    return new Candidate(
+                            it.getInt("id"),
+                            it.getString("name"),
+                            it.getString("description"),
+                            it.getTimestamp("created").toLocalDateTime()
+                    );
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
